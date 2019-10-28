d73068a1c8c092462cefa16e8e548a56c3fcaee8
Dmitry Gorbachuk <dmitry.gorbachuk@gmail.com>
2019-10-28 09:51:21 +0300

    some corrected

--- force-app/main/default/aura/ShowTimeComponent/ShowTimeComponentController.js
+++ force-app/main/default/aura/ShowTimeComponent/ShowTimeComponentController.js
@@ -1,11 +1,5 @@
 ({
     doInit: function (component, event, helper) {
-        // helper.TIME_OFFSET = component.get('v.timeOffset');
-        // console.log(typeof helper.TIME_OFFSET);
-        // console.log(typeof component.get('v.timeOffset'));
-        // console.log(helper.TIME_OFFSET);
         helper.showTime(component);
-        //console.log(component.get('v.timeOffset'));
-        
     }
 })
